0f25c4fed4233a63a4ffa059d7645dc1b5b55635
Andrew Cassidy <drewcassidy@me.com>
2014-07-07 15:22:23 -0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 update-rc.d lightdm enable 2 ;
 sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=.*/autologin-user=pi/" ;
 sudo apt-get update ;
+sudo wget http://goo.gl/Pthh62 -O isgh5.sh ;
+isgh5.sh ;
