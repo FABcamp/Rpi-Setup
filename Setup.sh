d5d1ee13f046e7914cc3e9ca886165f6eff57700
Andrew Cassidy <drewcassidy@me.com>
2014-07-07 15:36:00 -0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -2,5 +2,5 @@
 update-rc.d lightdm enable 2 ;
 sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=.*/autologin-user=pi/" ;
 sudo apt-get update ;
-sudo wget http://goo.gl/Pthh62 -O isgh5.sh ;
-sudo bash isgh5.sh ;
+sudo apt-get upgrade ;
+sudo bash ./setup/scratchGPIOinstaller.sh ;
