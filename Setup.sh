693b8beb774d0fb217fc6faf715d0d434921cfaa
Andrew Cassidy <drewcassidy@me.com>
2014-07-07 16:01:37 -0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -3,4 +3,4 @@ update-rc.d lightdm enable 2 ;
 sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=.*/autologin-user=pi/" ;
 sudo apt-get update ;
 sudo apt-get upgrade ;
-sudo bash ./setup/scratchGPIOinstaller.sh ;
+sudo bash ./setup/ScratchGPIOInstaller.sh ;
